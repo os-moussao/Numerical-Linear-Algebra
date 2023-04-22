73f203f584df91f9bd3da15a93f456a5479d40c7
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-04-22 22:48:01 +0000

    Update main

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,9 +1,16 @@
 #include "Matrix.hpp"
 
 int main() {
-	int n, m; cin >> n >> m;
-	Matrix<double> a(n, n);
+	int n; cin >> n;
+	Matrix<int> a(n, n);
 	cin >> a;
+
 	cout << "a =\n" << a << endl << endl;
 	cout << "determinant of a = " << a.determinant() << endl;
+
+	Matrix<double> b(n, n);
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			b[i][j] = a[i][j];
+	cout << "determinant of b = " << b.determinant() << endl;
 }

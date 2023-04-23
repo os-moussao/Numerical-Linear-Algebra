777c48be0c34977c9348136016103151ff1945ec
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-04-23 19:47:03 +0100

    Update test

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -6,11 +6,9 @@ int main() {
 	cin >> a;
 
 	cout << "a =\n" << a << endl << endl;
-	cout << "determinant of a = " << a.determinant() << endl;
+	cout << "determinant of a = " << a.determinant() << endl << endl;
 
-	Matrix<double> b(n, n);
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			b[i][j] = a[i][j];
+	Matrix<double> b; b = a;
+	cout << "b =\n" << b << endl << endl;
 	cout << "determinant of b = " << b.determinant() << endl;
 }
